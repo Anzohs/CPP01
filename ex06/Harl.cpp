3ea38c5fb0e7fb884088fc853ba2c2a9989cd375
Hugo Emanuel Ladeiro Nobre <10549215+Anzohs@users.noreply.github.com>
2025-04-04 11:06:49 +0000

    Last exercice creation!

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -11,3 +11,25 @@
 /* ************************************************************************** */
 
 #include "Harl.h"
+
+Harl::Harl(void){};
+
+Harl::~Harl(){};
+
+void Harl::debug(void){std::cout << DEBUG << std::endl;}
+void Harl::info(void){std::cout << INFO << std::endl;}
+void Harl::warning(void){std::cout << WARNING << std::endl;}
+void Harl::error(void){std::cout << ERROR << std::endl;}
+
+void Harl::complain(std::string level)
+{
+	std::string l[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	func f[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++){
+		if (level == l[i])
+		{
+			(this->*(f[i]))();
+		}
+	}
+}
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -14,5 +14,33 @@
 
 int	main(int ac, string av[])
 {
+	(void)ac;
+	(void)av;
+
+	std::string level[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int j;
+	if (ac > 2 || ac < 2)
+	{
+		std::cerr << "This program takes an argument\n";
+		return (1);
+	}
+
+	Harl tester = Harl();
+	j = 0;
+	for (int i=0; i < 4; i++){
+		if (level[i] == av[1])
+		{
+			j = i;
+			break;
+		}
+		if (i == 3 && j== 0)
+			j = 4;
+	}
+	for (;j < 4; j++)
+	{
+		std::cout << "[ " << level[j] << " ]" << std::endl;
+		tester.complain(level[j]);
+		std::cout << std::endl;
+	}
 	return (1);
 }
